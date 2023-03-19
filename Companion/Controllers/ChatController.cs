f3a8a232894b5b6f0b0947400923efcd42dc0443
Stephen Montgomery <stephen.montgomery@varian.com>
2023-03-19 15:34:23 +0200

    Add project files.

---
+++ Companion/Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Companion.Controllers;
+
+[ApiController]
+[Route("/")]
+public class CompanionController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public CompanionController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ChatResponse>> Post(ChatRequest request)
+    {
+        var api = new OpenAI_API.OpenAIAPI(_config["OpenAI:ApiKey"]);
+        var result = await api.Completions.GetCompletion(request.Question);
+
+        var chatResponse = new ChatResponse(result);
+
+        return chatResponse;
+    }
+}
+
+public record ChatRequest(string Question);
+
+public record ChatResponse(string Answer);
---
+++ Companion/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureSwaggerGen(setup =>
+{
+    setup.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Companion",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
